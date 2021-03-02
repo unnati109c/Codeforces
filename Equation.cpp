97f0e900a9c8e4f3b7846a3b882082bcbeb3d6e4
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-03-02 15:24:48 +0530

    Create Equation.cpp

---
+++ Equation.cpp
@@ -0,0 +1,19 @@
+//  https://codeforces.com/contest/1269/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ 
+ 
+ 
+ 
+int main() 
+{
+
+	ll n;
+	cin>>n;
+	
+	cout<<n * 10<<" "<<n * 9<<endl;
+
+	return 0;
+}
