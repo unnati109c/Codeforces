c7860e5cfff04645a1b7e9070ef0a190aaa69467
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-03-04 14:18:16 +0530

    Create Design Tutorial: Learn from Math.cpp

---
+++ Design Tutorial: Learn from Math.cpp
@@ -0,0 +1,16 @@
+//  https://codeforces.com/contest/472/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ long long int n;
+ cin>>n;
+ if(n%2==0)
+ cout<<8<<" "<<n-8;
+ else 
+ cout<<9<<" "<<n-9<<endl;
+ 
+ return 0;
+}
+ 
